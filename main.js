1afa98d226438d32cee2f925438b019247bd76c9
Denys Sttrilchuk <denia876@gmail.com>
2024-01-10 10:12:56 +0200

    continue doing my mini-project on JS

--- main.js
+++ main.js
@@ -26,18 +26,27 @@
 //     дати фон. марджини і тд)
 
 
-fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(values => {
-    console.log(values);
-    values.forEach(value => {
-        const userDiv = document.createElement('div');
-        const userInfo = document.createElement('p');
-        userInfo.innerText = `${value.id} - ${value.name}`;
-        const btn = document.createElement('button');
-        btn.innerText = 'details';
-        btn.onclick = function () {
-            document.location.href = 'user-details.html?userId=' + JSON.stringify(value);
-        }
-        userDiv.append(userInfo, btn);
-        document.body.appendChild(userDiv);
-    })
-})
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(values => {
+        const userContainer = document.querySelector('.user-container');
+
+        values.forEach(value => {
+            const userBlock = document.createElement('div');
+            userBlock.classList.add('user-block');
+
+            const userInfo = document.createElement('p');
+            userInfo.classList.add('user-info');
+            userInfo.innerText = `${value.id} - ${value.name}`;
+
+            const btn = document.createElement('button');
+            btn.classList.add('user-btn');
+            btn.innerText = 'Details';
+            btn.onclick = function () {
+                document.location.href = 'user-details.html?userId=' + JSON.stringify(value);
+            }
+
+            userBlock.append(userInfo, btn);
+            userContainer.appendChild(userBlock);
+        });
+    });
